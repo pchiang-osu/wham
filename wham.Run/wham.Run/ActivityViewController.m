f9a3ec23abdadee11c32c3c42fdbec89e48bb64b
Rutger Farry <rutgerfarry@gmail.com>
2015-02-03 22:48:01 -0800

    Switched location data to Mapbox's built in method
    Deleted my Core Location code and switched to Mapox's built-in method for location tracking. Although it might give up too much control over accuracy, I'd like to think that it will work, as it really cleans up the code.
    
    Also added an app icon and switched from creating the mapview in code to creating it in the storyboard.

--- wham.Run/wham.Run/ActivityViewController.m
+++ wham.Run/wham.Run/ActivityViewController.m
@@ -8,13 +8,12 @@
 
 #import "ActivityViewController.h"
 #import <Mapbox-iOS-SDK/Mapbox.h>
-#import <CoreLocation/CoreLocation.h>
+
+#define wMapID @"rutgerfarry.kpo3d7oo"
 
 @interface ActivityViewController () <CLLocationManagerDelegate>
 
-@property (nonatomic) RMMapboxSource *mapboxSource;
-@property (nonatomic) RMMapView *mapView;
-@property (nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) IBOutlet RMMapView *mapView;
 
 @end
 
@@ -22,77 +21,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.title = @"wham";
     [self setupMap];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    NSLog(@"Tabbar depth: %@", [self.view subviews]);
+    [self setupMap];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 
 #pragma mark - Setup
-
-- (void)startLocationUpdates
-{
-    CLLocationManager *locationManager = self.locationManager;
-    
-    // Authorization checks
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined)
-        [locationManager requestAlwaysAuthorization];
-    
-    // Alert user if they disabled location services
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted ||
-        [CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
-    {
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Location Services Disabled"
-                                                                                 message:@"Location Services are necessary for recording your workout. Please enable permission for wham.Run in Settings > Privacy > Location Services" preferredStyle:UIAlertControllerStyleAlert];
-        [alertController addAction:[UIAlertAction actionWithTitle:@"Okay"
-                                                            style:UIAlertActionStyleDefault
-                                                          handler:nil]];
-        
-        [self presentViewController:alertController animated:YES completion:^{
-            [self performSegueWithIdentifier:@"HomeViewController" sender:self];
-        }];
-    }
-    
-    // Enable appropriately accurate location services
-    if (!locationManager)
-        locationManager = [[CLLocationManager alloc] init];
-    
-    locationManager.delegate = self;
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    
-    locationManager.distanceFilter = 20.0;
-}
 
 - (void)setupMap
 {
     [RMConfiguration sharedInstance].accessToken = @"pk.eyJ1IjoicnV0Z2VyZmFycnkiLCJhIjoic3duRWdCRSJ9.18LzIGX1vQO-GTjgUYQt7A";
-    self.mapboxSource = [[RMMapboxSource alloc] initWithMapID:@"rutgerfarry.kpo3d7oo"];
-    self.mapView = [[RMMapView alloc] initWithFrame:[UIScreen mainScreen].bounds
-                                      andTilesource:self.mapboxSource];
-    [self.view addSubview:self.mapView];
-    [self.view sendSubviewToBack:self.mapView];
-    
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(44.565, -123.282);
-    [self.mapView setZoom:15.0 atCoordinate:coordinate animated:YES];
+    self.mapView.tileSource = [[RMMapboxSource alloc] initWithMapID:wMapID];
+    self.mapView.userTrackingMode = RMUserTrackingModeFollow;
+    self.mapView.zoom = 15.0;
 }
 
 @end
