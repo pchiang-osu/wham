c8bf66e31fb0e39b61dcb3f135b81dc258d2513b
enje <gabearon1987@gmail.com>
2015-03-21 17:48:59 -0700

    got openGL working for cube

---
+++ Yoga/YogaApp/LeftGLKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeftGLKViewController.h
+//  YogaApp
+//
+//  Created by CS Team on 3/21/15.
+//  Copyright (c) 2015 CS Team. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface LeftGLKViewController : GLKViewController
+
+@end
---
+++ Yoga/YogaApp/LeftGLKViewController.m
@@ -0,0 +1,37 @@
+//
+//  LeftGLKViewController.m
+//  YogaApp
+//
+//  Created by CS Team on 3/21/15.
+//  Copyright (c) 2015 CS Team. All rights reserved.
+//
+
+#import "LeftGLKViewController.h"
+
+@interface LeftGLKViewController ()
+
+@end
+
+@implementation LeftGLKViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
